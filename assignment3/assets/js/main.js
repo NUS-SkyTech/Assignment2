4511edf485e0c86cfaf411a601ece01d6cd0b862
Kyros Koh <i.am@kyroskoh.com>
2018-10-24 19:08:43 +0800

    Update main.js

--- assignment3/assets/js/main.js
+++ assignment3/assets/js/main.js
@@ -1,7 +1,7 @@
 /*
-	Twenty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Easier by SkyTech
+	nus-skytech.github.io | @kyroskoh
+	Free for personal and commercial use under the CCA 3.0 license
 */
 
 (function($) {
